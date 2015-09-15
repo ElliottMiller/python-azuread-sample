6cf6fd99fa3c30034c155e005c47e46b6b43ba6c
Elliott Miller <emil@microsoft.com>
2015-09-15 14:31:51 -0700

    Azure Auth Sample Initial commit

---
+++ AzureAuthSample/urls.py
@@ -0,0 +1,19 @@
+"""
+Definition of urls for AzureAuthSample.
+"""
+
+from datetime import datetime
+from django.conf.urls import patterns, url, include
+from app.forms import BootstrapAuthenticationForm
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url('', include('django.contrib.auth.urls', namespace='auth')),
+    url(r'^$', 'app.views.home', name='home'),
+)
---
+++ app/views.py
@@ -0,0 +1,23 @@
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+            'request': request,
+            'user': request.user,
+        })
+    )
